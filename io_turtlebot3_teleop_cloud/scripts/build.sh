1d6f9352514e6c172e0e02e7bc7c8d4e66323cc9
Gowtham S <gowtham.sundara@rapyuta-robotics.com>
2020-12-15 22:19:19 +0530

    Add the autobah backport

--- io_turtlebot3_teleop_cloud/scripts/build.sh
+++ io_turtlebot3_teleop_cloud/scripts/build.sh
@@ -3,4 +3,6 @@ cd ui
 apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_13.x | bash -
 apt-get install -y nodejs
+apt-get install -y python-pip
+pip install git+git://github.com/rapyuta-robotics/autobahn-python@py2_backport
 npm install
