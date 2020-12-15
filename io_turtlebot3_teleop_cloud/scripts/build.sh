f566c82f028636149091fb46b551e779e2ce77a3
Gowtham S <gowtham.sundara@rapyuta-robotics.com>
2020-12-15 15:40:35 +0530

    Add in submodule and change manifests

--- io_turtlebot3_teleop_cloud/scripts/build.sh
+++ io_turtlebot3_teleop_cloud/scripts/build.sh
@@ -3,6 +3,4 @@ cd ui
 apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_13.x | bash -
 apt-get install -y nodejs
-apt-get install -y python-pip
-pip install autobahn==20.12.2
 npm install
